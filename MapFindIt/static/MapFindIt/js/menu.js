6fe368ba7d3cfa2237d711ff6d88e8cc00dbe71b
vitorricoy <vitorricoy@yahoo.com.br>
2017-06-05 23:56:19 -0300

    Adicionada filtragem de amigos

--- MapFindIt/static/MapFindIt/js/menu.js
+++ MapFindIt/static/MapFindIt/js/menu.js
@@ -3,6 +3,7 @@ $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
 });
 var selec=false;
+
 $('#menu-toggle').on('click', function(e){
   if(selec){
     selec=false;
@@ -15,4 +16,15 @@ $('#menu-toggle').on('click', function(e){
       //Para o edge
       $('#sidebar-wrapper').css('overflow-y','scroll');
   }
+});
+$('#filtrarAmigos').keyup(function(event) {
+    $(".amigo").each(function(i, item){
+      item=$(item);
+      let anchor = item.children(":first").children(":first");
+      if(anchor.attr('title').indexOf($("#filtrarAmigos").val())!==-1){
+        item.show();
+      }else{
+        item.hide();
+      }
+    });
 });
